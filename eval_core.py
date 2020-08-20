f06bdea772ad4ade32173b542c892853e0930993
Antonin Deniau <adeniau@afoneinfrastructure.com>
2020-08-20 17:27:50 +0200

    tco

--- eval_core.py
+++ eval_core.py
@@ -12,26 +12,26 @@ def fn_symbol(ast, env):
     if (Name("&") in ast[1]) and (ast[1].index(Name("&")) != len(ast[1]) - 2):
         raise Exception("Function should contain only one variadic argument")
 
-    return lambda *e: evl(ast[2], Env(env, ast[1], e))
+    return lambda *e: evl(ast[2], Env(env, ast[1], e)), env
 
 def if_symbol(ast, env):
     if len(ast) < 3: return None
     res_cond = evl(ast[1], env)
 
-    if type(res_cond) == bool and res_cond == True: return evl(ast[2], env)
-    if type(res_cond) == int: return evl(ast[2], env)
-    if type(res_cond) == float: return evl(ast[2], env)
-    if type(res_cond) == list: return evl(ast[2], env)
-    if type(res_cond) == tuple: return evl(ast[2], env)
-    if type(res_cond) == str: return evl(ast[2], env)
+    if type(res_cond) == bool and res_cond == True: return ast[2], env
+    if type(res_cond) == int: return ast[2], env
+    if type(res_cond) == float: return ast[2], env
+    if type(res_cond) == list: return ast[2], env
+    if type(res_cond) == tuple: return ast[2], env
+    if type(res_cond) == str: return ast[2], env
 
-    return evl(ast[3], env) if len(ast) >= 4 else None
+    return ast[3], env if len(ast) >= 4 else None, env
 
 def do_symbol(ast, env):
     res = None
-    for x in ast[1::]:
+    for x in ast[1:-1]:
         res = evl(x, env)
-    return res
+    return ast[-1], env
 
 def let_symbol(ast,env):
     if not isinstance(ast[1], tuple) and not isinstance(ast[1], list): raise Exception("Not a list or vector {}".format(ast[1]))
@@ -45,11 +45,12 @@ def let_symbol(ast,env):
         data = evl(i[1], new_env)
         new_env.set(i[0], data)
 
-    return evl(ast[2], new_env)
+    return ast[2], new_env
 
 def eval_function(ast, env):
     ev = eval_ast(ast, env)
-    return ev[0](*ev[1::])
+    print(ev)
+    return ev[0](*ev[1::]), env
 
 def def_symbol(ast, env):
     if not isinstance(ast[1], Name): raise Exception("Not a symbol {}".format(ast[1]))
@@ -58,24 +59,25 @@ def def_symbol(ast, env):
 
     value = evl(ast[2], env)
     env.set(ast[1].name, value)
-    return value
+    return value, env
 
 ### EVAL PART ###
 
 def evl(ast, env):
-    if isinstance(ast, tuple):
-        if len(ast) == 0: return ast
+    while True:
+        if isinstance(ast, tuple):
+            if len(ast) == 0: return ast
 
-        if isinstance(ast[0], Name):
-            if ast[0].name == "def!": return def_symbol(ast,env)
-            if ast[0].name == "let*": return let_symbol(ast,env)
-            if ast[0].name == "do":   return do_symbol(ast,env)
-            if ast[0].name == "if":   return if_symbol(ast,env)
-            if ast[0].name == "fn*":  return fn_symbol(ast,env)
+            if isinstance(ast[0], Name):
+                if ast[0].name == "def!": ast, env = def_symbol(ast,env); continue
+                if ast[0].name == "let*": ast, env = let_symbol(ast,env); continue
+                if ast[0].name == "do":   ast, env = do_symbol(ast,env); continue
+                if ast[0].name == "if":   ast, env = if_symbol(ast,env); continue
+                if ast[0].name == "fn*":  ast, env = fn_symbol(ast,env); continue
 
-        return eval_function(ast, env)
+            ast, env = eval_function(ast, env); continue
 
-    return eval_ast(ast, env)
+        return eval_ast(ast, env)
 
 def eval_ast(ast, env):
     if isinstance(ast, dict):
@@ -90,4 +92,4 @@ def eval_ast(ast, env):
     if isinstance(ast, tuple):
         return tuple([evl(x, env) for x in ast])
 
-    return ast
+    return ast, env
