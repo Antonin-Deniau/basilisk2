a503c4250035ac65f50ed06748273290734dcf93
Antonin Deniau <antonin.deniau@protonmail.com>
2020-10-25 14:50:32 +0100

    init go

---
+++ src/core.go
@@ -0,0 +1,136 @@
+def with_meta(e, meta):
+    return e
+
+def readline(e):
+    try:
+        return input(e)
+    except BaseException:
+        return None
+
+def read_string(a):
+    try: 
+        return parse(a)
+    except UnexpectedToken as e:
+        raise BaslException("EOF/Parsing error around: \n" + e.get_context(a, 200))
+    except UnexpectedInput as e:
+        raise BaslException("EOF/Parsing error around: \n" + e.get_context(a, 200))
+    except Exception as e:
+        raise BaslException(e)
+
+def prn(*a):
+    print(" ".join([display(i, True) for i in a]))
+    return None
+
+def println(*a):
+    print(" ".join([display(i, False) for i in a]))
+    return None
+
+def equality(a, b):
+    if (type(a) == tuple or type(a) == list) and (type(b) == tuple or type(b) == list):
+        if len(a) != len(b): return False
+        for i in zip(a, b):
+            if not equality(i[0], i[1]): return False
+        return True
+
+    if type(a) == dict and type(b) == dict:
+        for k in a.keys():
+            if k not in b: return False
+
+            bv = b[k]
+            av = a[k]
+            if not equality(bv, av): return False
+        return True
+
+    return type(a) == type(b) and a == b
+
+def swap(a, b, *c):
+    if isinstance(b, Fn):
+        return a.reset(b.fn(a.data, *c))
+    else:
+        return a.reset(b(a.data, *c))
+
+def raiz(e):
+    raise BaslException(e)
+
+def ret_func(a):
+    if isinstance(a, Fn):
+        return a.fn
+    else:
+        return a
+
+def appl(a, *b):
+    is_arr = lambda x: isinstance(x, tuple) or isinstance(x, list)
+    args = reduce(lambda acc, arr: [*acc, *arr] if is_arr(arr) else [*acc, arr], b, [])
+    return ret_func(a)(*args)
+
+def basl_map(a, b):
+    s = map(ret_func(a), b)
+    return tuple(s)
+
+def pr_str(*a):
+    return " ".join([display(i, True) for i in a])
+
+ns = {
+    '+': lambda a,b: a+b,
+    '-': lambda a,b: a-b,
+    '*': lambda a,b: a*b,
+    '/': lambda a,b: int(a/b),
+    'list': lambda *a: tuple(a),
+    'list?': lambda a: isinstance(a, tuple),
+    'empty?': lambda a: len(a) == 0,
+    'count': lambda a: 0 if a == None else len(a),
+    '=': equality,
+    '<': lambda a, b: a < b,
+    '<=': lambda a, b: a <= b,
+    '>=': lambda a, b: a >= b,
+    '>': lambda a, b: a > b,
+    'pr-str': pr_str,
+    'str': lambda *a: "".join([display(i, False) for i in a]),
+    'prn': prn,
+    'println': println,
+    'read-string': read_string,
+    'slurp': lambda a: open(a, "r").read(),
+    'atom': lambda a: Atom(a),
+    'atom?': lambda a: isinstance(a, Atom),
+    'deref': lambda a: a.data if isinstance(a, Atom) else nil,
+    'reset!': lambda a, b: a.reset(b) if isinstance(a, Atom) else nil,
+    'swap!': swap,
+    'cons': lambda a, b: tuple([a,*b]),
+    'concat': lambda *a: tuple(reduce(lambda acc, arr: [*acc, *arr], a, ())),
+    'vec': lambda a: list(a) if isinstance(a, list) or isinstance(a, tuple) else [a],
+    'nth': lambda a, i: a[i],
+    'first': lambda a: a[0] if a != None and len(a) != 0 else None,
+    'rest': lambda a: tuple(a[1:]) if a != None and len(a) != 0 else tuple(),
+    'throw': raiz,
+    'apply': appl,
+    'map': basl_map,
+    'nil?': lambda e: isinstance(e, type(None)),
+    'true?': lambda e: e == True,
+    'false?': lambda e: e == False,
+    'symbol?': lambda e: isinstance(e, Name),
+    'symbol': lambda e: Name(e),
+    'keyword': lambda e: Keyword(e.name if isinstance(e, Keyword) else e),
+    'keyword?': lambda e: isinstance(e, Keyword),
+    'vector': lambda *e: list([*e]),
+    'vector?': lambda e: isinstance(e, list),
+    'sequential?': lambda e: isinstance(e, list) or isinstance(e, tuple),
+    'hash-map': lambda *e: { k[0]: k[1] for k in zip(e[::2], e[1::2]) },
+    'map?': lambda e: isinstance(e, dict),
+    'assoc': lambda a, *e: {**a, **{ k[0]: k[1] for k in zip(e[::2], e[1::2]) } },
+    'dissoc': lambda e, *di: {key: val for key, val in e.items() if key not in di },
+    'get': lambda e, k: e.get(k, None) if e is not None else None,
+    'contains?': lambda e, k: k in e,
+    'keys': lambda e: tuple(e.keys()),
+    'vals': lambda e: tuple(e.values()),
+    'time-ms': lambda: int(round(time.time() * 1000)),
+    'number?': lambda e: (isinstance(e, float) or isinstance(e, int)) and not isinstance(e, bool),
+    'string?': lambda e: isinstance(e, str),
+    'fn?': lambda e: (isinstance(e, Fn) and e.is_macro == False) or isinstance(e, types.LambdaType),
+    'macro?': lambda e: isinstance(e, Fn) and e.is_macro,
+    'conj': lambda e, *rest: tuple([*reversed(rest), *e]) if isinstance(e, tuple) else list([*e, *rest]),
+    'quux-eval': lambda e: eval(e),
+    'readline': readline,
+    'meta': lambda e: e.meta if hasattr(e, "meta") else None,
+    'with-meta': with_meta,
+    'seq': lambda e: tuple(e) if e != None and len(e) != 0 else None,
+}
---
+++ src/parser.go
@@ -0,0 +1,177 @@
+rules=r'''
+?start: obj |
+
+?obj: list
+    | metadata
+    | deref
+    | hashmap
+    | vector
+    | keyword
+    | quote
+    | quasiquote
+    | spliceunquote
+    | unquote
+    | python
+    | COMMA
+    | TOKEN -> name
+    | COMMENT
+    | NUM -> number
+    | BOOLEAN -> boolean
+    | string
+    | variadic
+    | NIL -> nil
+
+list: "(" obj* ")"
+metadata: "^" obj obj
+deref: "@" obj
+hashmap: "{" ((keyword|string) obj)* "}"
+vector: "[" obj* "]"
+keyword: ":" TOKEN
+quote: "'" obj
+quasiquote: "`" obj
+unquote: "~" obj
+spliceunquote: "~@" obj
+python: "\." TOKEN
+string: ESCAPED_STRING
+variadic: "&"
+
+NIL.5: /nil(?!\?)/
+BOOLEAN.5: /(true|false)(?!\?)/
+NUM.5: "-"?NUMBER
+
+COMMENT: /;.*(?=(\n|$))/
+COMMA: ","
+
+TOKEN: /[^"^.@~`\[\]:{}&'0-9\s,();][^"^@~`\[\]:{}\s();]*/
+
+ESCAPED_STRING: /"(\\.|[^"\\])*"/
+
+%import common.NUMBER
+%import common.WS
+%ignore WS
+%ignore COMMENT
+%ignore COMMA
+'''
+
+l = Lark(rules, parser='lalr', start="start")
+
+def unescape(s):
+    res =  ""
+    esc = False
+    for i in s:
+        if i == '\\' and esc == False:
+            esc = True
+        elif i == '\\' and esc == True:
+            res += "\\"
+            esc = False
+        elif i == "n" and esc == True:
+            res += "\n"
+            esc = False
+        elif i == '"' and esc == True:
+            res += '"'
+            esc = False
+        else:
+            res += i
+            esc = False
+
+    return res
+
+def escape(s):
+    res =  ""
+    for i in s:
+        if i == "\\":
+            res += "\\\\"
+        elif i == '"':
+            res += '\\"'
+        elif i == '\n':
+            res += '\\n'
+        else:
+            res += i
+
+    return res
+
+def pr_str(x, readably):
+    return escape(x) if readably else x
+
+class ToAst(Transformer):
+    start = lambda _,x: x[0] if len(x) else None
+    list = tuple
+    vector = lambda _,x: list(x)
+
+    nil = lambda _,x: None
+    variadic = lambda _,x: Name("&")
+    number = lambda _,x: float(x[0].value) if x[0].value.find(".") != -1 else int(x[0].value) 
+    boolean = lambda _,x: x[0] == "true"
+    name = lambda _,x: Name(x[0].value)
+    string = lambda _, x: unescape(x[0][1:-1])
+    deref = lambda _,x: tuple([Name("deref"), *x])
+    metadata = lambda _,x: tuple([Name("with-meta"), x[1], x[0]])
+    hashmap = lambda _,x: { i[0]: i[1] for i in zip(list(x[::2]), list(x[1::2])) }
+    keyword = lambda _,x: Keyword(x[0].value)
+    quote = lambda _,x: tuple([Name("quote"), *x])
+    quasiquote = lambda _,x: tuple([Name("quasiquote"), *x])
+    unquote = lambda _,x: tuple([Name("unquote"), *x])
+    spliceunquote = lambda _,x: tuple([Name("splice-unquote"), *x])
+
+def display(x, readably):
+    if isinstance(x, bool):
+        return "true" if x is True else "false"
+
+    if isinstance(x, types.LambdaType):
+        return "#<function>"
+
+    if isinstance(x, Fn):
+        return "#<function>"
+
+    if isinstance(x, tuple):
+        return "({})".format(" ".join([display(r, readably) for r in x]))
+
+    if isinstance(x, int):
+        return repr(x)
+
+    if isinstance(x, float):
+        return repr(x)
+
+    if isinstance(x, str):
+        return "\"{}\"".format(pr_str(x, readably)) if readably else x
+
+    if isinstance(x, list):
+        return "[{}]".format(" ".join([display(s, readably) for s in x]))
+
+    if isinstance(x, dict):
+        return "{{{}}}".format(
+                " ".join(["{} {}".format(display(k, readably), display(v, readably)) for k,v in x.items()]))
+
+    if isinstance(x, Keyword):
+        return ":{}".format(x.name)
+
+    if isinstance(x, BaslException):
+        return display(x.message, readably)
+
+    if isinstance(x, Name):
+        return x.name
+
+    if isinstance(x, Atom):
+        return "(atom {})".format(display(x.data, readably))
+
+    if x is None:
+        return "nil"
+
+    return x
+
+def parse(data):
+    tree = l.parse(data)
+    return ToAst().transform(tree)
+
+def prnt(e):
+    sys.stdout.write(display(e, True))
+    sys.stdout.write("\n")
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            res = prnt(parse(input("basilisk> ")))
+            print(res if res != None else "nil")
+        except Exception as e:
+            print("EOF: {}".format(e))
